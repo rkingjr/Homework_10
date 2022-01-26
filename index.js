00b3694ed5c1775ee3d5af215c175f6bc8e0994c
Randy King Jr <rkingjr@outlook.com>
2022-01-26 09:48:32 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Manager = require(`./lib/Manager`);
+const Engineer = require(`./lib/Engineer`);
+const Intern = require(`./lib/Intern`);
+const Inquirer = require(`inquirer`);
+const path = require(`path`);
+const fs = require(`fs`);
+
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
+
+const render = require(`./src/page-template.js`);
+
+const teamMembers = [];
+const idArray = [];
