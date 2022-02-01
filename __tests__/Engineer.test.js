498f02439ef72cf7ad8d904c8007c4bfdf687e42
Randy King Jr <rkingjr@outlook.com>
2022-02-01 16:50:43 -0600

    update test files

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Bill", 1, "test@test.com", testValue, "Engineer");
+    expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Bill", 1, "test@test.com", "GitHubUser", "Engineer");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Bill", 1, "test@test.com", testValue, "Engineer");
+    expect(e.getGithub()).toBe(testValue);
+});
